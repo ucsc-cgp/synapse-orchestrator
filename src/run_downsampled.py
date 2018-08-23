5b18784731fbadd30729a1849b38a6d37e30c86a
Lon Blauvelt <lblauvel@ucsc.edu>
2018-08-23 13:02:56 -0700

    Comment rogue script.

--- src/run_downsampled.py
+++ src/run_downsampled.py
@@ -1,4 +1,8 @@
-from synorchestrator.orchestrator import set_queue_from_user_json, run_all  # , run_submission
+from synorchestrator.orchestrator import set_queue_from_user_json, run_all
+
+# An example script to be turned into a commandline script later.
+
+# from synorchestrator.orchestrator import run_submission
 # run_submission("Argon-Globus", "140814152850149946")
 
 set_queue_from_user_json('tests/data/user_submission_example.json')
